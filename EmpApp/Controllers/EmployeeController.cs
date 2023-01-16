c8b91337580214f59abd3c7a773bc207ee95fefa
Shikha Srivastava <shikhasri3020@gmail.com>
2023-01-16 23:21:32 +0530

    business logic layer added

---
+++ EmpApp/Controllers/EmployeeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+// using BOL;
+// using BLL;
+
+namespace EmpApp.Controllers;
+
+public class EmployeeController : Controller
+{
+    private readonly ILogger<EmployeeController> _logger;
+
+    public EmployeeController(ILogger<EmployeeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
